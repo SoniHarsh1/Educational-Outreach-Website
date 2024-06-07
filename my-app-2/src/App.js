61f694cbcac9e5a9a13210f836fb29c25cc1a3b8
Sarthak3611 <sarthakindurkhya03@gmail.com>
2024-06-07 21:24:10 +0530

    Added files

---
+++ my-app-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Picscard from './components/Picscard';
+
+
+function App() {
+  return (
+    <>
+         <Picscard/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Bottom from './components/Bottom';
+import Cards from './components/Cards';
+import TitlePage from './components/TitlePage';
+
+function App() {
+  return (
+         <>
+           <TitlePage />
+           <Cards/>
+           <Bottom/>
+         </>
+  );
+}
+
+export default App;
