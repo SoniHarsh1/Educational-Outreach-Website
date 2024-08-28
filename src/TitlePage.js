0cf5ccbb20c7f8a892e9864f24a71e37cd5191f8
Harsh <98403303+SoniHarsh1@users.noreply.github.com>
2024-08-29 00:48:05 +0530

    patch - 1

--- src/TitlePage.js
+++ src/TitlePage.js
@@ -10,7 +10,7 @@ export default function TitlePage() {
           <div className="container">
             <div className="row">
                 <div className="col">
-                         <p  className='display-6 text-center text-info font-weight-normal' id='head'>Welcome to Education Outreach Initiative of IIT Indore!</p>
+                         <p  className='display-6 text-center text-info font-weight-normal' id='head'>Welcome to Educational Outreach Initiative of IIT Indore!</p>
                          <p className='my-1 text-center font-weight-normal' id='head'><b>Empowering Community to Learn, Grow, and 'Thrive</b></p>
                 </div>
             </div>           
